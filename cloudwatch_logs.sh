fbb8191bf6dd6182f1672a97b687d48c5a05c140
Rendell Arruda <rendellarruda3@gmail.com>
2023-12-05 13:34:46 -0300

    add rds, cloudwatch-log and vopc

--- cloudwatch_logs.sh
+++ cloudwatch_logs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Array com os nomes dos grupos de log que você deseja etiquetar
-nomes_log_groups=("nome-log-group-1" "nome-log-group-2" "nome-log-group-3")
+nomes_log_groups=("log-group-taguear" "log-group-taguear2")
 
 # Loop para adicionar as tags aos grupos de log do CloudWatch
 for log_group in "${nomes_log_groups[@]}"; do
-    output=$(aws logs tag-log-group --log-group-name "$log_group" --tags Key=Produto,Value=infraestrutura Key=Subproduto,Value=infraestrutura 2>&1)
+    output=$(aws logs tag-log-group --log-group-name "$log_group" --tags Produto=infra2,Subproduto=infra2 2>&1)
 
     if [[ $? -eq 0 ]]; then
         echo "Tags adicionadas ao grupo de log $log_group"
