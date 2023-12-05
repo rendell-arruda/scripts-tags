1ff4e7e67f3993fac3ceaad74820453466e336f8
Rendell Arruda <rendellarruda3@gmail.com>
2023-12-05 14:41:40 -0300

    update kms

--- kms.sh
+++ kms.sh
@@ -5,7 +5,7 @@ ids_chaves_kms=("id-chave-kms-1" "id-chave-kms-2" "id-chave-kms-3")
 
 # Loop para adicionar as tags às chaves do KMS
 for id_chave in "${ids_chaves_kms[@]}"; do
-    output=$(aws kms tag-resource --key-id "$id_chave" --tags TagKey1=ValorTag1 TagKey2=ValorTag2 2>&1)
+    output=$(aws kms tag-resource --key-id "$id_chave" --tags TagKey='Produto',TagValue='Test' TagKey='Subproduto',TagValue='IT' 2>&1)
 
     if [[ $? -eq 0 ]]; then
         echo "Tags adicionadas à chave do KMS $id_chave"
